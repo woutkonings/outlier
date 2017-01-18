ac29d57aad5c5f2973c7e6d20dc9f1bb5ef7e94c
CasperS <c.j.schroder@student.tudelft.nl>
2017-01-18 22:45:53 +0100

    Added basic functionality Scout

--- src/woutfirstattempt/Scout.java
+++ src/woutfirstattempt/Scout.java
@@ -3,6 +3,7 @@ import battlecode.common.*;
 
 public class Scout extends Globals
 {
+	static float offset = 0;
 	public static void loop()
 	{
 		System.out.println("I am veeery fast!");
@@ -15,12 +16,36 @@ public class Scout extends Globals
 				//Updates the number of scouts.
         		int prev = Message.getNumberOfType("SCOUT");
         		rc.broadcast(Message.SCOUT_CHANNEL, prev+1);
+        		
+        		Navigation.flee();
+        		scouting(offset);
+        		
 			}
 			catch(Exception e)
 			{
 				System.out.println("Scout Exception");
 				e.printStackTrace();
 			}
+		}
+	}
+	/**
+	 * Keep moving in a direction until it is no longer possible, then move to a different direction
+	 * @param o : the angle in which the scout moves
+	 */
+	public static void scouting(float o)
+	{
+		try
+		{
+		
+			Direction dir = new Direction(0);
+			if (!Navigation.tryMove(dir, o, 2)){
+				offset = (float) Math.random() * 360;
+			}
+			
+		}
+		catch(GameActionException e)
+		{
+			e.printStackTrace();
 		}
 	}
 }
